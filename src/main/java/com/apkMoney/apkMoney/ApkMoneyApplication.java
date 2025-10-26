1f8d1d632a940b8f38746c3c0691ecd01538076e
Jean Baptiste <jeanbaptistedielinang@gmail.com>
2025-10-26 01:39:48 +0100

    Initial commit

---
+++ src/main/java/com/apkMoney/apkMoney/ApkMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.apkMoney.apkMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApkMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apkMoney/apkMoney/ApkMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apkMoney.apkMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApkMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
